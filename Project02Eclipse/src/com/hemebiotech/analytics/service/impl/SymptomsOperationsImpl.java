9fa11548684f054945381b807ed57582bda2b681
fredbuono <frederic.buono.devweb@gmail.com>
2022-09-15 18:09:08 +0200

    some refacto

--- Project02Eclipse/src/com/hemebiotech/analytics/service/impl/SymptomsOperationsImpl.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/service/impl/SymptomsOperationsImpl.java
@@ -12,11 +12,25 @@ public class SymptomsOperationsImpl implements ISymptomsOperations {
     @Override
     public Map<String, Long> symptomsAndCount(List<String> symptoms){
         return symptoms.stream()
+                // List<String> become a stream and we could use .collect
                 .collect(Collectors.groupingByConcurrent(s -> s, Collectors.counting()))
+                // .entrySet() give us a set view of mapping fever = 3 etc...
                 .entrySet().stream()
-                .sorted(Map.Entry.comparingByKey())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                // .stream in order to use method .sorted by the key, here symptom
+                .sorted(Map.Entry.comparingByValue())
+                // then collect
+                .collect(Collectors.toMap(
+                        // implements Function IF to mapp
+                        stringLongEntry -> stringLongEntry.getKey(),
+                        // implements Function IF to mapp
+                        o -> o.getValue(),
+                        // implements Function IF BinaryOperator to merge
+                        (oldValue, newValue) -> oldValue,
+                        // implements Function IF Supplier , it return the new LinkedHashMap
+                        () -> new LinkedHashMap<>())
+                );
+
+
     }
 
 }
